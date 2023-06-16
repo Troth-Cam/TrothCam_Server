176b4cc03867d8592ccce0389643be8e2be92e1e
hojeong2747 <hojeong2747@gmail.com>
2023-06-17 01:05:52 +0900

    add: spring boot

---
+++ src/main/java/trothly/trothcam/TrothcamApplication.java
@@ -0,0 +1,13 @@
+package trothly.trothcam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrothcamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrothcamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trothly/trothcam/TrothcamApplicationTests.java
@@ -0,0 +1,13 @@
+package trothly.trothcam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrothcamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
