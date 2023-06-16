f3dd1933412f33105ceedb22da30a81948ca83da
hojeong2747 <hojeong2747@gmail.com>
2023-06-17 02:29:20 +0900

    add: SampleController, fix: build.gradle

---
+++ src/main/java/trothly/trothcam/SampleController.java
@@ -0,0 +1,13 @@
+package trothly.trothcam;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping("/sample")
+    public String sample() {
+        return "Sample!!";
+    }
+}
