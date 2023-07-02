05ee47933469d6f6458aef96106561e260e95fb0
aeeazip <codnjs0809@naver.com>
2023-07-02 19:53:02 +0900

    fix(#5) : import 수정 + 환경변수 추가

--- src/main/java/trothly/trothcam/feign/AppleClaimsValidator.java
+++ src/main/java/trothly/trothcam/feign/AppleClaimsValidator.java
@@ -1,7 +1,8 @@
 package trothly.trothcam.feign;
 
 import io.jsonwebtoken.Claims;
-import lombok.Value;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +10,8 @@ public class AppleClaimsValidator {
     private final String iss;
     private final String clientId;
 
+    // iss = apple 개발자 계정 team id (발급자)
+    // client-id = app bundle id
     public AppleClaimsValidator(
             @Value("${oauth.apple.iss}") String iss,
             @Value("${oauth.apple.client-id}") String clientId
