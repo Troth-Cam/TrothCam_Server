f299d4b78e4ce16842d4e72d6a6042d53906a86d
hojeong2747 <hojeong2747@gmail.com>
2023-07-10 23:06:32 +0900

    feat(#3) : 구글 로그인 사용자 정보 받아오기까지

--- src/main/java/trothly/trothcam/dto/auth/google/GoogleOauthToken.java
+++ src/main/java/trothly/trothcam/dto/auth/google/GoogleOauthToken.java
@@ -1,2 +1,16 @@
-package trothly.trothcam.dto.auth.google;public class GoogleOauthToken {
+package trothly.trothcam.dto.auth.google;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class GoogleOauthToken {
+    private String access_token;
+    private int expires_in;
+    private String scope;
+    private String token_type;
+    private String id_token;
 }
--- src/main/java/trothly/trothcam/dto/auth/google/GoogleUser.java
+++ src/main/java/trothly/trothcam/dto/auth/google/GoogleUser.java
@@ -1,2 +1,18 @@
-package trothly.trothcam.dto.auth.google;public class GoogleUser {
+package trothly.trothcam.dto.auth.google;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class GoogleUser {
+    private String id;
+    private String email;
+    private Boolean verified_email;
+    private String name;
+    private String given_name;
+    private String picture;
+    private String locale;
 }
