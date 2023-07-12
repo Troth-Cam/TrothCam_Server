7e6cdb58308db529578c9b2c3cded5d0f94ad0d0
hojeong2747 <hojeong2747@gmail.com>
2023-07-12 21:50:00 +0900

    feat(#3) : 구글 로그인 토큰 값 DB 저장 -> 성공

--- src/main/java/trothly/trothcam/service/auth/OAuthService.java
+++ src/main/java/trothly/trothcam/service/auth/OAuthService.java
@@ -115,7 +115,8 @@ public class OAuthService {
 
     // code -> accessToken 받아오기
     // accessToken -> 사용자 정보 받아오기
-    public void oauthLogin(String socialLoginType, String code) throws JsonProcessingException {
+    @Transactional
+    public LoginResDto oauthLogin(String socialLoginType, String code) throws JsonProcessingException {
         // 구글로 일회성 코드를 보내 액세스 토큰이 담긴 응답객체를 받아옴
         ResponseEntity<String> accessTokenResponse = googleOauth.requestAccessToken(code);
 
@@ -127,5 +128,39 @@ public class OAuthService {
 
         // 다시 JSON 형식의 응답 객체를 deserialization 해서 자바 객체에 담음
         GoogleUser googleUser = googleOauth.getUserInfo(userInfoResponse);
+        String email = googleUser.getEmail();
+        log.info(googleUser.getEmail());
+
+        if(email == null)
+            throw new BaseException(ErrorCode.NOT_AGREE_EMAIL);
+
+        Optional<Member> getMember = memberRepository.findByEmail(email);
+        Member member;
+        if(getMember.isPresent()){  // 이미 회원가입한 회원인 경우
+            member = getMember.get();
+            if(!member.getProvider().equals(Provider.GOOGLE))   // 이미 회원가입했지만 GOOGLE이 아닌 다른 소셜 로그인 사용
+                throw new InvalidProviderException("APPLE로 회원가입한 회원입니다.");
+        } else {    // 아직 회원가입 하지 않은 회원인 경우
+            member = memberRepository.save(
+                    Member.builder()
+                            .email(email)
+                            .provider(Provider.GOOGLE)
+                            .build()
+            );
+        }
+
+        // accessToken, refreshToken 발급
+        String newAccessToken = jwtService.encodeJwtToken(new TokenDto(member.getId()));
+        String newRefreshToken = jwtService.encodeJwtRefreshToken(member.getId());
+
+        // redis에 refreshToken 저장
+//        redisTemplate.opsForValue().set(member.getId().toString(), newRefreshToken, 14L, TimeUnit.SECONDS);
+//        log.info("redis에 저장된 refreshToken : " + newRefreshToken + "\nmember.getId : " + member.getId().toString());
+
+        // DB에 refreshToken 저장
+        member.updateRefreshToken(newRefreshToken);
+        memberRepository.save(member);
+        return new LoginResDto(newAccessToken, newRefreshToken);
+
     }
 }
