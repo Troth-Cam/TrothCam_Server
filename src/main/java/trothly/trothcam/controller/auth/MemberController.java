c885670e1ad4526433f7b8223e17a41fed56cfc2
Juhee <viimv2000@gmail.com>
2023-07-17 17:58:24 +0900

    feat: BaseResponse 적용

--- src/main/java/trothly/trothcam/controller/auth/MemberController.java
+++ src/main/java/trothly/trothcam/controller/auth/MemberController.java
@@ -2,37 +2,42 @@ package trothly.trothcam.controller.auth;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import trothly.trothcam.domain.member.Member;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import trothly.trothcam.dto.auth.signup.CheckIdRes;
 import trothly.trothcam.dto.auth.signup.SignupReq;
 import trothly.trothcam.dto.auth.signup.SignupRes;
+import trothly.trothcam.exception.base.BaseResponse;
 import trothly.trothcam.service.auth.MemberService;
-
-import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
-@Slf4j
+@RequestMapping("/auth")
 public class MemberController {
 
     private final MemberService memberService;
 
     /* 웹 회원 가입 */ /* 500 에러 왜..? */
-    @PostMapping("/auth/signup")
-    public SignupRes signup(@RequestBody SignupReq req) {
-        String webId = memberService.signup(req);
-        return new SignupRes(webId);
+    @PostMapping("/signup")
+    public BaseResponse<SignupRes> signup(@RequestBody @Validated SignupReq req, BindingResult bindingResult) throws Exception {
+        if (bindingResult.hasErrors()) {
+            ObjectError objectError = bindingResult.getAllErrors().stream().findFirst().get();
+            return BaseResponse.onFailure(400, objectError.getDefaultMessage(), null);
+        }
+        SignupRes result = memberService.signup(req);
+        return BaseResponse.onSuccess(result);
     }
 
-    /* 아이디 중복 확인 */ /* 근데 왜 모든 값에 대해 false 가 뜨는지? */
-    @PostMapping("/auth/check-id/{id}")
-    public CheckIdRes checkDuplicateId(@PathVariable String id) {
-        boolean isDuplicated = memberService.checkDuplicateId(id);
-        return new CheckIdRes(id, isDuplicated);
+    /* 아이디 중복 확인 */ /* 500 에러?! */
+    @PostMapping("/check-id/{id}")
+    public BaseResponse<CheckIdRes> checkDuplicateId(@PathVariable String id, BindingResult bindingResult) throws Exception {
+        if (bindingResult.hasErrors()) {
+            ObjectError objectError = bindingResult.getAllErrors().stream().findFirst().get();
+            return BaseResponse.onFailure(400, objectError.getDefaultMessage(), null);
+        }
+        CheckIdRes result = memberService.checkDuplicateId(id);
+        return BaseResponse.onSuccess(result);
     }
 }
--- src/main/java/trothly/trothcam/service/auth/MemberService.java
+++ src/main/java/trothly/trothcam/service/auth/MemberService.java
@@ -6,7 +6,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import trothly.trothcam.domain.member.Member;
 import trothly.trothcam.domain.member.MemberRepository;
+import trothly.trothcam.dto.auth.signup.CheckIdRes;
 import trothly.trothcam.dto.auth.signup.SignupReq;
+import trothly.trothcam.dto.auth.signup.SignupRes;
+import trothly.trothcam.exception.base.BaseException;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,22 +20,23 @@ public class MemberService {
 
     /* 회원 가입 */
     @Transactional
-    public String signup(SignupReq signupReq) {
+    public SignupRes signup(SignupReq signupReq) {
         System.out.println("web token: " + signupReq.getWebToken());
         Member findMember = memberRepository.findByWebToken(signupReq.getWebToken());
         System.out.println("findMember: " + findMember.toString());
         findMember.updateMember(signupReq.getWebToken(), signupReq.getWebId(), signupReq.getWebPassword(), signupReq.getName(), signupReq.getPhone(), signupReq.getEmail());
 
-        return findMember.getWebId();
+        return new SignupRes(findMember.getWebId());
     }
 
     /* 아이디 중복 확인 */
     @Transactional(readOnly = true)
-    public boolean checkDuplicateId(String webId) {
+    public CheckIdRes checkDuplicateId(String webId) {
+        boolean isDuplicated = false;
         System.out.println("아이디 중복 확인" + webId + memberRepository.existsByWebId(webId));
         if (memberRepository.existsByWebId(webId)) {
-            return true;
+            isDuplicated = true;
         }
-        return false;
+        return new CheckIdRes(webId, isDuplicated);
     }
 }
