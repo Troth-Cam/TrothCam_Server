92265e42e8298bdd0d6fc3a90d027eb795a24285
yeon015 <lizkitty@naver.com>
2023-08-11 02:37:08 +0900

    feat(#46) : image 수정 및 product, history, likeProduct 생성

--- src/main/java/trothly/trothcam/domain/image/Image.java
+++ src/main/java/trothly/trothcam/domain/image/Image.java
@@ -3,6 +3,8 @@ package trothly.trothcam.domain.image;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import trothly.trothcam.domain.Share;
+import trothly.trothcam.domain.core.BaseTimeEntity;
 import trothly.trothcam.domain.member.Member;
 
 import javax.persistence.*;
@@ -11,7 +13,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "image")
-public class Image {
+public class Image extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "image_id")
@@ -26,6 +28,25 @@ public class Image {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @Column(name = "image_share", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Share share;
+
+    @Column(name = "image_content", nullable = true)
+    private String content;
+
+    @Column(name = "image_lens", nullable = true)
+    private String lens;
+
+    @Column(name = "image_location", nullable = true)
+    private String location;
+
+    @Column(name = "image_resolution", nullable = true)
+    private String resolution;
+
+    @Column(name = "image_size", nullable = true)
+    private String size;
 
     public Image(String imageHash, Member member) {
         this.imageHash = imageHash;
