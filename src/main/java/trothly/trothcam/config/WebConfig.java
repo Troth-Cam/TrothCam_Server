692e73f520c9836d669d94998e7583bd670600c6
aeeazip <codnjs0809@naver.com>
2023-08-23 23:30:29 +0900

    fix(#125) : Webconfig 수정

--- src/main/java/trothly/trothcam/config/WebConfig.java
+++ src/main/java/trothly/trothcam/config/WebConfig.java
@@ -11,11 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:8080","http://localhost:3000", "https://trothly.com") // 허용할 출처
+                .allowedOrigins("http://localhost:8080","http://localhost:3000", "https://trothly.com") // 허용할 출처
                 .allowedMethods("*") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .allowedHeaders("*")
-                .allowedOrigins("*")
                 .maxAge(3600L); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
 }
